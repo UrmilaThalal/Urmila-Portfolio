e322d1d87ce2219820b513515423dc9524463d83
Urmila Thalal <utsabithalal@gmail.com>
2026-03-09 17:18:24 +0545

    initial commit

---
+++ html-css-js-example/js/script.js
@@ -0,0 +1,6 @@
+function showMessage(){
+
+document.getElementById("message").innerHTML =
+" desktop tablet mobile fluid layout flexible images --- media queries --- one mobile first approach";
+
+}
